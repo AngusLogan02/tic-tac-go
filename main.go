ebea70dcc3a588a8d37f86809bd9cb0ee6cbb167
Angus Logan <angus.logan@baronsview.com>
2022-06-21 16:18:37 +0100

    basic landing page

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"tic-tac-go/handlers"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
-	r.LoadHTMLGlob("public/html")
-	r.Static("/static/css", "public/css")
+	r.LoadHTMLGlob("public/html/*")
+	r.Static("/static/css", "./public/css")
+	r.Static("/static/js", "./public/js")
+
+	r.GET("/", handlers.IndexHandler)
+
+	r.Run()
 }
